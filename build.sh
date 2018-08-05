57278044bfd84c86c2288e9fa951c0851d661e01
phanmduong <phanminhduong@hotmail.com>
2018-08-05 19:22:37 +0700

    ADD build at Sun Aug  5 19:22:37 +07 2018

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ FILENAME=colorme
 
 rm -rf ${DIR_OUTPUT}
 mkdir ${DIR_OUTPUT}
-go build -o ${DIR_OUTPUT}${FILENAME} main.go
-cp -r ./public/. ${DIR_OUTPUT}public
+GOOS=linux GOARCH=amd64 go build -o ${DIR_OUTPUT}${FILENAME} -v main.go
 cp env.example ${DIR_OUTPUT}
+cp -r ./public/. ${DIR_OUTPUT}public
+cd ${DIR_OUTPUT}
+git add .
+git commit -a -m "ADD build at `date`"
+git push origin master
+cd ..
