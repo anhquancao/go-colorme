91ff1d630a323b51156ef4decdbcf8a9ab0f65c6
phanmduong <phanminhduong@hotmail.com>
2018-08-07 14:49:04 +0700

    update code

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-DIR_OUTPUT=./build/
+DIR_OUTPUT=build/
 FILENAME=colorme
 
-rm -rf ${DIR_OUTPUT}public ${DIR_OUTPUT}${FILENAME} ${DIR_OUTPUT}env.example
+rm -rf ${DIR_OUTPUT}
+git clone https://github.com/phanmduong/go-colorme-build
+mv go-colorme-build ${DIR_OUTPUT}
 GOOS=linux GOARCH=amd64 go build -o ${DIR_OUTPUT}${FILENAME} -v main.go
 cp env.example ${DIR_OUTPUT}
 cp -r ./public/. ${DIR_OUTPUT}public
 cd ${DIR_OUTPUT}
 git add .
 git commit -a -m "ADD build at `date`"
-git push origin master
+git push -u origin master
 cd ..
